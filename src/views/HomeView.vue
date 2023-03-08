c319b2314b072adca5ae3992333092a803839e5f
Sahar Hallaji <saharhallaji.dev@gmail.com>
2023-03-08 23:53:28 +0330

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Tweets from "../components/Tweets.vue"
+
+let tweets = [{
+  name: "Steve Schogere",
+  username: "Steve_Schoger",
+  date: "Jun 27",
+  tweet: "🔥 If you're tired of using outline styles for secondary buttons, a soft solid background based on the text color can be a great alternative.",
+  comments:33,
+  retweets : 96,
+  likes: 2600
+}]
+
+</script>
+
+<template>
+  <main>
+
+  </main>
+</template>
